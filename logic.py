fb24d80208c84a45403728c9e38b3efa1f19d22d
hixxly <monste4ek@yandex.ru>
2025-02-02 19:07:22 +0300

    feedupdate

--- logic.py
+++ logic.py
@@ -1,96 +1,109 @@
 from random import randint
-import requests
+import requests, datetime
+from datetime import timedelta
+
+class Trainer:
+    def __init__(self, username):
+        self.username = username
 
 class Pokemon:
-    pokemons = {} # { username : pokemon}
-    # Инициализация объекта (конструктор)
-    def __init__(self, pokemon_trainer):
+    pokemons = {}
 
-        self.pokemon_trainer = pokemon_trainer   
-
-        self.pokemon_number = randint(1,1000)
+    def __init__(self, trainer):
+        self.lasttime = datetime.now()
+        self.trainer = trainer
+        self.pokemon_number = randint(1, 1000)
         self.img = self.get_img()
         self.name = self.get_name()
-
+        self.weight, self.height = self.get_weight_height()
+        self.happiness = 0 
         self.power = randint(30, 60)
         self.hp = randint(200, 400)
+        Pokemon.pokemons[trainer.username] = self
 
-        Pokemon.pokemons[pokemon_trainer] = self
-
-    # Метод для получения картинки покемона через API
     def get_img(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
         response = requests.get(url)
         if response.status_code == 200:
-            data = response.json()
-            return (data['sprites']["other"]['official-artwork']["front_default"])
-        else:
-            return "https://static.wikia.nocookie.net/anime-characters-fight/images/7/77/Pikachu.png/revision/latest/scale-to-width-down/700?cb=20181021155144&path-prefix=ru"
+            return response.json()['sprites']['front_default']
+        return None
     
-    # Метод для получения имени покемона через API
     def get_name(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
         response = requests.get(url)
         if response.status_code == 200:
+            return response.json()['forms'][0]['name']
+        return "Pikachu"
+
+    def get_weight_height(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
             data = response.json()
-            return (data['forms'][0]['name'])
+            return data['weight'], data['height']
+        return None, None
+
+    def feed(self, feed_interval = 20, hp_increase = 10 ):
+        current_time = datetime.now()  
+        delta_time = timedelta(second = feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.lasttime = current_time
+            return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp}"
         else:
-            return "Pikachu"
+            return f"Следующее время кормления покемона: {self.lasttime + delta_time}"  
 
-
-    # Метод класса для получения информации
     def info(self):
-        return f"""Имя твоего покеомона: {self.name}
-Cила покемона: {self.power}
-Здоровье покемона: {self.hp}"""
-
-    # Метод класса для получения картинки покемона
+        return (
+            f"Имя покемона: {self.name}, "
+            f"Вес: {self.weight}, Рост: {self.height}, "
+            f"Уровень счастья: {self.happiness}, "
+            f"Сила: {self.power}, Здоровье: {self.hp}, "
+            f"Тренер: {self.trainer.username}"
+        )
+    
     def show_img(self):
         return self.img
-    
+
     def attack(self, enemy):
-        if isinstance(enemy, Wizard):
-            chance = randint(1,5)
-            if chance == 1:
-                return "Покемон-волшебник применил щит в сражении"
-        if enemy.hp > self.power:
-            enemy.hp -= self.power
-            return f"""Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}
-Здоровье @{enemy.pokemon_trainer} теперь {enemy.hp}"""
+        if enemy.hp > 0:
+            damage = min(enemy.hp, self.power)  # Убедимся, что здоровье не станет отрицательным
+            enemy.hp -= damage
+            return f"""Сражение @{self.trainer.username} с @{enemy.trainer.username}
+Здоровье @{enemy.trainer.username} теперь {enemy.hp}"""
         else:
-            enemy.hp = 0
-            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+            return f"Покемон @{enemy.trainer.username} уже побежден!"
 
 class Wizard(Pokemon):
-    """Подкласс Pokemon, представляющий покемона-волшебника."""
-
     def info(self):
-        """Возвращает информацию о покемоне-волшебнике."""
         return "У тебя покемон-волшебник \n\n" + super().info()
-class Fighter(Pokemon):
-    """Подкласс Pokemon, представляющий покемона-бойца."""
 
+class Fighter(Pokemon):
     def attack(self, enemy):
-        """Атака с использованием супер-удара."""
-        super_power = randint(5, 15)  # Генерируем дополнительную силу атаки
-        self.power += super_power  # Увеличиваем силу атаки перед ударом
-        result = super().attack(enemy)  # Вызываем стандартную атаку
-        self.power -= super_power  # Возвращаем силу атаки к изначальному значению
-        return result + f"\nБоец применил супер-атаку силой: {super_power} "
+        super_power = randint(5, 15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nБоец применил супер-атаку силой: {super_power}"
 
     def info(self):
-        """Возвращает информацию о покемоне-бойце."""
         return "У тебя покемон-боец \n\n" + super().info()
-# Создаём двух покемонов: волшебника и бойца
-wizard = Wizard("username1")
-fighter = Fighter("username2")
 
-# Выводим информацию о покемонах
+# Пример использования
+trainer1 = Trainer("Ash")
+trainer2 = Trainer("Misty")
+
+wizard = Wizard(trainer1)
+fighter = Fighter(trainer2)
+
 print(wizard.info())
 print("#" * 10)
 print(fighter.info())
 print("#" * 10)
 
-# Проводим сражение между покемонами
 print(wizard.attack(fighter))
 print(fighter.attack(wizard))
+
+# Кормим покемонов
+wizard.feed()
+fighter.feed()
--- main.py
+++ main.py
@@ -1,44 +1,61 @@
 import telebot 
 from config import token
 from random import randint
-from logic import *
+from logic import Pokemon, Wizard, Fighter  # Убедитесь, что вы импортируете необходимые классы
 
 bot = telebot.TeleBot(token) 
 
 @bot.message_handler(commands=['go'])
 def start(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
-        chance = randint(1,3)
+        chance = randint(1, 3)
         if chance == 1:
             pokemon = Pokemon(message.from_user.username)
         elif chance == 2:
             pokemon = Wizard(message.from_user.username)
         elif chance == 3:
             pokemon = Fighter(message.from_user.username)
+        
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
     else:
-        bot.reply_to(message, "Ты уже создал себе покемона")
-
+        bot.reply_to(message, "Ты уже создал себе покемона.")
 
 @bot.message_handler(commands=['attack'])
 def attack_pok(message):
     if message.reply_to_message:
-        if message.reply_to_message.from_user.username in Pokemon.pokemons.keys() and message.from_user.username in Pokemon.pokemons.keys():
-            enemy = Pokemon.pokemons[message.reply_to_message.from_user.username]
-            pok = Pokemon.pokemons[message.from_user.username]
+        enemy_username = message.reply_to_message.from_user.username
+        user_username = message.from_user.username
+        
+        if enemy_username in Pokemon.pokemons.keys() and user_username in Pokemon.pokemons.keys():
+            enemy = Pokemon.pokemons[enemy_username]
+            pok = Pokemon.pokemons[user_username]
             res = pok.attack(enemy)
             bot.send_message(message.chat.id, res)
         else:
-            bot.send_message(message.chat.id, "Сражаться можно только с покемонами")
+            bot.send_message(message.chat.id, "Сражаться можно только с покемонами.")
     else:
-            bot.send_message(message.chat.id, "Чтобы атаковать, нужно ответить на сообщения того, кого хочешь атаковать")
+        bot.send_message(message.chat.id, "Чтобы атаковать, нужно ответить на сообщение того, кого хочешь атаковать.")
 
 @bot.message_handler(commands=['info'])
 def info(message):
+    # Проверяем, есть ли у пользователя покемон
     if message.from_user.username in Pokemon.pokemons.keys():
-            pok = Pokemon.pokemons[message.from_user.username]
-            bot.send_message(message.chat.id, pok.info())
+        # Получаем покемона пользователя
+        pok = Pokemon.pokemons[message.from_user.username]
+        # Отправляем информацию о покемоне
+        bot.send_message(message.chat.id, pok.info())
+    else:
+        bot.send_message(message.chat.id, "У тебя нет покемона. Создай его с помощью команды /go.")
 
+@bot.message_handler(commands=['feed'])
+def feed_pokemon(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+        pok = Pokemon.pokemons[message.from_user.username]
+        pok.feed()  # Метод кормления покемона
+        bot.send_message(message.chat.id, f"{pok.name} был покормлен! Уровень счастья: {pok.happiness}")
+    else:
+        bot.send_message(message.chat.id, "У тебя нет покемона. Создай его с помощью команды /go.")
 
+# Запуск бота
 bot.infinity_polling(none_stop=True)
